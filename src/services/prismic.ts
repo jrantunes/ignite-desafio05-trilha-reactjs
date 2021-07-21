78576cc6607aee3503da8004dd2ade98d1a89324
José Neto <joseantunes2001@gmail.com>
2021-07-21 14:05:03 -0300

    added prismic access token

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -3,7 +3,7 @@ import { DefaultClient } from '@prismicio/client/types/client';
 
 export function getPrismicClient(req?: unknown): DefaultClient {
   const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
-    accessToken: 'MC5ZUGJadmhBQUFDRUEwYlVU.77-977-977-977-977-9QGTvv70o77-977-977-977-977-9Ue-_ve-_vQEfVTrvv73vv70j77-9MO-_ve-_vXvvv73vv73vv70',
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
     req,
   });
 
